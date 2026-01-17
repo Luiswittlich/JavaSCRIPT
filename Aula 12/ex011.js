6bbbc3cd17717037c905ecdc1c6f7f55f50e8d9d
Luis Carlos <luiscwj@gmail.com>
2026-01-17 20:39:18 -0300

    sss

--- Aula 12/ex011.js
+++ Aula 12/ex011.js
@@ -1,9 +1,10 @@
-var idade = 67
+var idade = 16
 console.log(`Você tem ${idade} anos!`)
-if (idade < 16) {
+if (idade < 16){
     console.log('Não vota!')
 } else if (idade < 18 || idade > 65){
-    console.log('Voto opcional!')
+    console.log(`Voto opcional!`)
 } else {
-    console.log('Voto obrigatório!')
+    console.log(`Voto obrigatório!`)
 }
+    
--- Aula 12/ex012.js
+++ Aula 12/ex012.js
@@ -1,7 +1,7 @@
 var agora = new Date()
 var hora = agora.getHours()
 console.log(`Agora são exatamente ${hora} horas!`)
-if (hora <12 && hora >= 8){
+if (hora < 12 && hora >= 8){
     console.log('Bom-dia!')
 } else if (hora <= 18 && hora >=12){
     console.log('Boa-tarde!')
